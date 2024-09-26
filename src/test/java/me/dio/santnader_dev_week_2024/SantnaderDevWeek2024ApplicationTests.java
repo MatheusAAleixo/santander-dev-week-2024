011446f2d689a9afa72d3e2c99112766a6d4ea3a
MatheusAAleixo <83460598+MatheusAAleixo@users.noreply.github.com>
2024-09-25 23:02:51 -0300

    Add files via upload
    Firts Commit

---
+++ src/test/java/me/dio/santnader_dev_week_2024/SantnaderDevWeek2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santnader_dev_week_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantnaderDevWeek2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
